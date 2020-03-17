5d62abc07475fce71e494f11210a8f0272f7cede
Ash Caire <ash.caire@gmail.com>
2020-03-17 18:09:30 +0800

    Improve template for generate command

--- stax/commands/cmd_generate.py
+++ stax/commands/cmd_generate.py
@@ -1,8 +1,10 @@
 '''
 Generate a sample stax.json file
 '''
-import click
 import json
+import os
+
+import click
 
 
 @click.command()
@@ -27,21 +29,30 @@ def generate():
         },
         default_region='ap-southeast-2',
         stacks={
-            'my_first_stack_with_no_params': {
+            'the_name_of_stack_1_that_uses_the_default_region': {
                 'parameters': {
                     'development': '',
                 },
-                'template': 'my_first_stack.json',
+                'template': 'my_stack_which_does_not_require_params.json',
             },
-            'my_second_stack_with_params': {
+            'the_name_of_stack_2_that_uses_the_default_region': {
                 'parameters': {
                     'staging': {
-                        'REDIS_USERNAME': 'example'
+                        'REDIS_USERNAME': 'this_is_an_example_param_value'
                     },
-                    'production': 'my_second_stack-prod.json'
+                    'production': 'this_is_how_to_use_a_file_instead.json'
                 },
                 'template': 'my_second_stack.json',
+            },
+            'the_name_of_stack_3_that_uses_a_specific_region': {
+                'parameters': {
+                    'development': "",
+                },
+                'region': 'us-east-1',
+                'template': 'my_third_stack_with_a_specific_region.json',
             }
         },
     )
     click.echo(json.dumps(content, sort_keys=True, indent=4))
+    if os.fstat(0) == os.fstat(1):
+        click.echo(click.style('Redirect to stax.json to save this file', bold=True))
